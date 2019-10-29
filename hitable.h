0d62622d30c12d0b3048813831e17419c866a4ec
Cass Everitt <cass@xyzw.us>
2019-10-29 17:36:49 -0500

    hitable collection

--- hitable.h
+++ hitable.h
@@ -3,6 +3,8 @@
 #include "linear.h"
 #include "ray.h"
 
+#include <vector>
+
 struct Hit
 {
 	typedef r3::Vec3f V;
@@ -14,4 +16,37 @@ struct Hit
 struct Hitable
 {
 	virtual bool Hits( const Ray& ray, float t_min, float t_max, Hit* hit ) = 0;
+};
+
+struct HitableCollection : public Hitable
+{
+	std::vector<Hitable*> hitables;
+
+	bool Hits( const Ray& ray, float t_min, float t_max, Hit* hit )
+	{
+		bool has_hit = false;
+		Hit closest_hit;
+		for ( auto hitable : hitables )
+		{
+			Hit h;
+			if ( hitable->Hits( ray, t_min, t_max, &h ) )
+			{
+				if ( has_hit == false )
+				{
+					closest_hit = h;
+					has_hit = true;
+					continue;
+				}
+				if ( h.t < closest_hit.t )
+				{
+					closest_hit = h;
+				}
+			}
+		}
+		if ( hit && has_hit )
+		{
+			*hit = closest_hit;
+		}
+		return has_hit;
+	}
 };
