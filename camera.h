77b47e1381dc0f4ec3529ef91f8d601a49ff11d4
Cass Everitt <cass@xyzw.us>
2019-10-29 22:19:10 -0500

    camera

---
+++ camera.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ray.h"
+#include <cmath>
+#include <stdio.h>
+
+struct Camera
+{
+	typedef r3::Vec2f V2;
+	typedef r3::Vec3f V3;
+
+	// symmetric fov
+	Camera( float fovy_deg, float aspect )
+	{
+		ur.y = tan( 0.5f * fovy_deg * M_PI / 180.0 );
+		ur.x = aspect * ur.y;
+		ll = -ur;
+	}
+
+	Ray GetRay( float u, float v )
+	{
+		V3 dir( r3::Lerp( ll.x, ur.x, u ), r3::Lerp( ll.y, ur.y, v ), -1 );
+		return Ray( V3( 0, 0, 0 ), dir );
+	}
+
+	V2 ll;
+	V2 ur;
+};
